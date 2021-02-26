dc51c5bf40ad84b8c4cac69211443a725972124f
Carlos Escobar <carlos.satoritech@gmail.com>
2021-02-25 22:11:46 -0600

    :panda_face: Retrofit Configuration

---
+++ web/src/main/java/mx/com/satoritech/web/APIDataLol.kt
@@ -0,0 +1,50 @@
+package mx.com.satoritech.web
+
+import com.google.gson.GsonBuilder
+import mx.com.satoritech.web.api.serializers.BooleanDeserializer
+import mx.com.satoritech.web.api.serializers.BooleanSerializer
+import mx.com.satoritech.web.api.serializers.DateDeserializer
+import mx.com.satoritech.web.api.serializers.DateSerializer
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+object APIDataLol {
+    private const val TAG = "API_DATA_LOL"
+//    private val apiService: DataLolService
+
+    init {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        //Prepare http client
+        val httpClientBuilder = OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        // Prepare Gson instance
+        val gson = GsonBuilder()
+            .registerTypeAdapter(Boolean::class.java, BooleanSerializer())
+            .registerTypeAdapter(Boolean::class.java, BooleanDeserializer())
+            .registerTypeAdapter(Boolean::class.javaPrimitiveType, BooleanSerializer())
+            .registerTypeAdapter(Boolean::class.javaPrimitiveType, BooleanDeserializer())
+            .registerTypeAdapter(Date::class.java, DateSerializer())
+            .registerTypeAdapter(Date::class.java, DateDeserializer())
+            .create()
+
+        //Prepare retrofit instance
+//        val retrofit = Retrofit.Builder()
+//                .baseUrl(APIConstants.serverPath)
+//                .addConverterFactory(ScalarsConverterFactory.create())
+//                .addConverterFactory(GsonConverterFactory.create(gson))
+//                .client(httpClietBuilder.build())
+//                .build()
+    }
+}
---
+++ web/src/main/java/mx/com/satoritech/web/DataLolService.kt
@@ -0,0 +1,10 @@
+package mx.com.satoritech.web
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface DataLolService {
+
+}
